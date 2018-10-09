4949f7012a0153a377b716809bb77dbd0b968d0c
delchev <delchevn@gmail.com>
2018-10-09 13:04:52 +0300

    rating fixed

--- vendorx-ecommerce-marketplace/product.js
+++ vendorx-ecommerce-marketplace/product.js
@@ -81,24 +81,11 @@ angular.module('product').controller('ProductController', function ($scope, $htt
 	loadReviews();
 	
 
-
-    $scope.rate = 7;
-    $scope.max = 10;
-    $scope.isReadonly = false;
-  
     $scope.hoveringOver = function(value) {
         $scope.overStar = value;
         $scope.percent = 100 * (value / $scope.max);
     };
-
-    $scope.ratingStates = [
-        {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
-        {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
-        {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
-        {stateOn: 'glyphicon-heart'},
-        {stateOff: 'glyphicon-off'}
-    ];
-
+    
     $scope.openNewDialog = function() {
 		$scope.actionType = 'new';
 		$scope.entity = {};
